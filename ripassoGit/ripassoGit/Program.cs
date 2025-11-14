bb0bb719ed064f74a579112e2067c3e2e43e93c8
Luca Gaetano Bonasia <LUCAGAETANO.BONASIA@local.ittcesena.it>
2025-11-14 08:38:29 +0100

    Versione base

---
+++ ripassoGit/ripassoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ripassoGit
+{
+    internal class Program
+    {
+        static string path = "file.txt";
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
