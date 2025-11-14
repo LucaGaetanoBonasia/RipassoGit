21ca964a50b94c2b8db16a93237753771d795adb
Luca Gaetano Bonasia <LUCAGAETANO.BONASIA@local.ittcesena.it>
2025-11-14 08:55:16 +0100

    Classe Read creata

--- ripassoGit/ripassoGit/Read.cs
+++ ripassoGit/ripassoGit/Read.cs
@@ -8,5 +8,20 @@ namespace ripassoGit
 {
     internal class Read
     {
+        public string path { get; set; }
+        public Read(string path)
+        {
+            this.path = path;
+        }
+        
+        public void Leggi()
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line= reader.ReadToEnd();
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("File letto con successo");           
+        }
     }
 }
